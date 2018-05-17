8fe95425e5d71f990061dd573e75596c024ea4f5
Arun Kadiveru <arun.kadiveru@ggktech.com>
2018-05-17 16:12:58 +0530

    login authentication using google

--- FitnessApp/app/login/login.component.ts
+++ FitnessApp/app/login/login.component.ts
@@ -1,20 +1,39 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
+import * as application from "application";
+import * as firebase from "nativescript-plugin-firebase";
+import * as frame from "ui/frame";
+
+import * as dialogs from "ui/dialogs";
+
+
+
+
 @Component({
     selector: "gr-login",
     templateUrl: "./login/login.component.html",
     styleUrls: ["./login/login.component.css"],
 })
 export class LoginComponent {
-
     constructor(private router: Router) {
 
     }
 
-    login() {
-
-        this.router.navigate(["dashboard"]);
+    public onGoogleLogin() {
+        firebase.login({
+            type: firebase.LoginType.GOOGLE,
+        }).then(user => {
+            console.log('Login success -- User details');
+            console.log(JSON.stringify(user));
+            this.router.navigate(["/dashboard"]);
+        }).catch(err => {
+            console.log(err);
+            dialogs.alert(err);
+        })
+    }
+    public logOut(){
+        firebase.logout();
     }
 
 }
--- FitnessApp/app/main.ts
+++ FitnessApp/app/main.ts
@@ -1,4 +1,22 @@
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
+import * as app from 'application';
+import * as appSettings from "application-settings";
+import * as firebase from "nativescript-plugin-firebase";
 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
+
+firebase.init({
+    onAuthStateChanged: function (data) { // optional but useful to immediately re-logon the user when he re-visits your app
+        console.log(data.loggedIn ? "Logged in to firebase" : "Logged out from firebase");
+        appSettings.setBoolean("loggedIn", data.loggedIn);
+
+        if (data.loggedIn) {
+            console.log("user's email address: " + (data.user.email ? data.user.email : "N/A"));
+        }
+    }
+}).then(instance => {
+    console.log("firebase.init done");
+}).catch(err => {
+    console.log("Firebase init error: " + err);
+});
