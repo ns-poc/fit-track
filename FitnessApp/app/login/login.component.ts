d6745655cdfc976f0dae8507c81ff0df4699a86d
Akhil Sai Bommarapu <akhil.bommarapu@ggktech.com>
2018-05-18 13:18:19 +0530

    native sript installation and removing back pagehistory once logged in

--- FitnessApp/app/login/login.component.ts
+++ FitnessApp/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { Router } from "@angular/router";
 import * as application from "application";
 import * as firebase from "nativescript-plugin-firebase";
 import * as frame from "ui/frame";
-
+import { RouterExtensions } from "nativescript-angular/router";
 import * as dialogs from "ui/dialogs";
 
 
@@ -16,17 +16,18 @@ import * as dialogs from "ui/dialogs";
     styleUrls: ["./login/login.component.css"],
 })
 export class LoginComponent {
-    constructor(private router: Router) {
+    constructor(private router: Router, private routerExtensions: RouterExtensions) {
 
     }
 
     public onGoogleLogin() {
+        console.log("hitdsnkjdf");
         firebase.login({
             type: firebase.LoginType.GOOGLE,
         }).then(user => {
             console.log('Login success -- User details');
             console.log(JSON.stringify(user));
-            this.router.navigate(["/dashboard"]);
+            this.routerExtensions.navigate(["/dashboard"],{ clearHistory: true });
         }).catch(err => {
             console.log(err);
             dialogs.alert(err);
